7712ad569fca4ebe1c5854aac86947aefe213611
Vladislav Kantaev <vvkantaev@yandex.ru>
2022-03-31 15:24:21 +0300

    Add gizmos for billboard grass mask

--- Assets/BillboardGrass/BillboardGrassMask.cs
+++ Assets/BillboardGrass/BillboardGrassMask.cs
@@ -26,16 +26,47 @@ namespace BillboardGrass
 
         private void Update()
         {
-            float2 position = ((float3)_position.position).xz;
-            position = position * _positionTilingOffset.xy + _positionTilingOffset.zw;
+            var position = WorldPositionToNormalized(_position.position);
             TryDraw(position, _positionSize);
             Graphics.Blit(_renderTexture1, _renderTexture2, _fadeMaterial);
             (_renderTexture1, _renderTexture2) = (_renderTexture2, _renderTexture1);
-            
+
             Shader.SetGlobalTexture("_BillboardGrassMask", _renderTexture1);
             Shader.SetGlobalVector("_BillboardGrassMask_ST", _positionTilingOffset);
         }
-        
+
+        private void OnDestroy()
+        {
+            DestroyRt(ref _renderTexture1);
+            DestroyRt(ref _renderTexture2);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+
+            var min = NormalizedPositionToWorld(0);
+            var max = NormalizedPositionToWorld(1);
+            var size = max - min;
+            var center = min + size * 0.5f;
+            Gizmos.DrawWireCube(center, size);
+        }
+
+        private float2 WorldPositionToNormalized(float3 positionWs)
+        {
+            var position = positionWs.xz;
+            position = position * _positionTilingOffset.xy + _positionTilingOffset.zw;
+            return position;
+        }
+
+        private float3 NormalizedPositionToWorld(float2 positionNormalized)
+        {
+            var position = positionNormalized.xxy;
+            position.y = 0f;
+            position.xz = (position.xz - _positionTilingOffset.zw) / _positionTilingOffset.xy;
+            return position;
+        }
+
         private void TryDraw(float2 normalizedPosition, float halfBrushSizeNormalized)
         {
             var activeRt = RenderTexture.active;
@@ -45,7 +76,7 @@ namespace BillboardGrass
             _stampMaterial.SetPass(0);
             GL.LoadOrtho();
             GL.Begin(GL.QUADS);
-            
+
             GL.TexCoord2(0, 0);
             GlVertex(normalizedPosition + new float2(-halfBrushSizeNormalized, -halfBrushSizeNormalized));
             GL.TexCoord2(0, 1);
@@ -60,16 +91,10 @@ namespace BillboardGrass
             GL.PopMatrix();
             RenderTexture.active = activeRt;
         }
-        
+
         private static void GlVertex(float2 vertex)
         {
             GL.Vertex(new Vector3(vertex.x, vertex.y));
-        }
-
-        private void OnDestroy()
-        {
-            DestroyRt(ref _renderTexture1);
-            DestroyRt(ref _renderTexture2);
         }
 
         private RenderTexture CreateRt() =>
